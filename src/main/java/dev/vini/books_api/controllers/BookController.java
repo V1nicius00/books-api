018f1293bdbeda36552f74f40d8ec3e00c7e54ce
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-07-13 21:10:00 -0300

    feat: creating createBook endpoint and service

--- src/main/java/dev/vini/books_api/controllers/BookController.java
+++ src/main/java/dev/vini/books_api/controllers/BookController.java
@@ -1,7 +1,25 @@
 package dev.vini.books_api.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import dev.vini.books_api.domain.book.Book;
+import dev.vini.books_api.domain.book.CreateBookDto;
+import dev.vini.books_api.services.BookService;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/books")
 public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody CreateBookDto dto){
+        return bookService.save(dto);
+    }
+
 }
--- src/main/java/dev/vini/books_api/services/BookService.java
+++ src/main/java/dev/vini/books_api/services/BookService.java
@@ -1,7 +1,27 @@
 package dev.vini.books_api.services;
 
+import dev.vini.books_api.domain.book.Book;
+import dev.vini.books_api.domain.book.CreateBookDto;
+import dev.vini.books_api.repositories.BookRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public ResponseEntity save(CreateBookDto dto){
+        Book newBook = new Book(dto);
+        bookRepository.save(newBook);
+        return ResponseEntity.ok(newBook);
+    }
+
 }
