cfb2fcb5e284e6df45cb406420f7708fb6c9f326
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-07-17 20:42:07 -0300

    feat: creating custom jpa query findBookByName

--- src/main/java/dev/vini/books_api/repositories/BookRepository.java
+++ src/main/java/dev/vini/books_api/repositories/BookRepository.java
@@ -4,8 +4,11 @@ import dev.vini.books_api.domain.book.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, UUID> {
+    List<Book> findByBookName(String bookName);
 }
