e5d4993ff5595407b53ecc057a56ba1c7d768ca6
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-07-19 21:22:11 -0300

    feat: creating jpa query for get book by price less than

--- src/main/java/dev/vini/books_api/repositories/BookRepository.java
+++ src/main/java/dev/vini/books_api/repositories/BookRepository.java
@@ -13,4 +13,5 @@ public interface BookRepository extends JpaRepository<Book, UUID> {
     List<Book> findByBookName(String bookName);
     List<Book> findByActiveTrue();
     List<Book> findByActiveFalse();
+    List<Book> findByPriceLessThan(Double price);
 }
