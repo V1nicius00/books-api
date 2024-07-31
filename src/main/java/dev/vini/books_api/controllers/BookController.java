a18fca03d00c8e8eea97a48453dde55b1e17cd39
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-07-31 20:13:32 -0300

    feat: adding cors config

--- src/main/java/dev/vini/books_api/controllers/BookController.java
+++ src/main/java/dev/vini/books_api/controllers/BookController.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/books")
+@CrossOrigin(origins = "http://localhost:4200")
 public class BookController {
 
     private final BookService bookService;
