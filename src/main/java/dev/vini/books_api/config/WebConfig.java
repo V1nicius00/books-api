bd97ce6831feee1391c7d62f8f1cafaa68d2071c
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-08-27 22:23:56 -0300

    feat: removing comments

--- src/main/java/dev/vini/books_api/config/WebConfig.java
+++ src/main/java/dev/vini/books_api/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")  // URL do frontend Angular
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
